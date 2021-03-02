69bb5f0f57c1a3c249b1bbc5a47a5a0f9df59431
Miika Savela <miika.a.savela@gmail.com>
2021-03-02 21:19:48 +0200

    Add updating tile coordinates automatically based on their position

---
+++ Assets/CoordinateHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateHandler : MonoBehaviour
+{
+    private TextMeshPro cubeText;
+    private Vector2Int coordinates = new Vector2Int();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cubeText = GetComponent<TextMeshPro>();
+        SetCubeText();
+        
+    }
+
+    void Update()
+    {
+       if (!Application.isPlaying)
+        {
+            SetCubeText();
+            SetGameObjectNameToCoordinate();
+        }
+    }
+
+    private void SetCubeText()
+    {
+        float grizSize = UnityEditor.EditorSnapSettings.move.x;
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / grizSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / grizSize); 
+        cubeText.text = $"{coordinates.x}, {coordinates.y}";
+    }
+
+    private void SetGameObjectNameToCoordinate()
+    {
+        gameObject.transform.parent.name = $"Tile_{coordinates.x}_{coordinates.y}";
+    }
+}
