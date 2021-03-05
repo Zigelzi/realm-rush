95411c84543b85a1d7417dc8df13ce561d7b9b62
Miika Savela <miika.a.savela@gmail.com>
2021-03-05 20:20:07 +0200

    Add Waypoint system and movement between waypoints

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+    [SerializeField] float stepTime = 1f; // Seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            MoveTo(waypoint);
+            yield return new WaitForSeconds(stepTime);
+        }
+    }
+
+    void MoveTo(Waypoint destination)
+    {
+        transform.position = destination.transform.position;
+    }
+}
