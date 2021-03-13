13b3fdc8fd48032bc0ceb4d75c0e1a8001457d2c
Miika Savela <miika.a.savela@gmail.com>
2021-03-13 20:19:29 +0200

    Add tower building behaviour and tower targetting behaviour

---
+++ Assets/Scripts/TargetLocator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private Transform weapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon = transform.Find("BallistaTopMesh").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        GameObject closestEnemy = GetTarget();
+        weapon.LookAt(closestEnemy.transform);
+    }
+
+    private GameObject GetTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy = enemies[0];
+        
+        Vector3 towerPosition = transform.position;
+        float closestEnemyDistance = Vector3.Distance(towerPosition, closestEnemy.transform.position);
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceFromTower = Vector3.Distance(towerPosition, enemy.transform.position);
+            if (distanceFromTower < closestEnemyDistance)
+            {
+                closestEnemy = enemy;
+            }
+        }
+        return closestEnemy;
+    }
+}
---
+++ Assets/Scripts/TowerBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] GameObject buildObjectPrefab;
+    [SerializeField] bool isPlaceable;
+
+    private bool hasBuilding = false;
+    private GameObject tower;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right click");
+            DestroyTower();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        BuildTower();
+    }
+
+    private void BuildTower()
+    {
+        if (buildObjectPrefab != null && !hasBuilding)
+        {
+            tower = Instantiate(buildObjectPrefab, transform.position, transform.rotation, transform);
+            hasBuilding = true;
+        }
+    }
+
+    private void DestroyTower()
+    {
+        if (tower != null && hasBuilding)
+        {
+            Destroy(tower);
+            hasBuilding = false;
+        }
+    }
+}
