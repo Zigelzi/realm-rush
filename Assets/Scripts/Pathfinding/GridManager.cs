c825d4fb7af43497ff41e8fee6c994b04d60bf59
Miika Savela <miika.a.savela@gmail.com>
2021-03-21 08:58:41 +0200

    Add basic pathfinding grid creation

---
+++ Assets/Scripts/Pathfinding/GridManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] Vector2Int gridSize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    void Awake()
+    {
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                AddNodeToGrid(x, y);
+            }
+        }
+    }
+
+    void AddNodeToGrid(int x, int y)
+    {
+        Vector2Int coordinates = new Vector2Int(x, y);
+        Node node = new Node(coordinates, true);
+        grid.Add(coordinates, node);
+        Debug.Log($"Node coordinates: {grid[coordinates].coordinates} and it's walkable: {grid[coordinates].isWalkable}");
+    }
+}
